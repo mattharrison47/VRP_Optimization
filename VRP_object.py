c8c42ea07c72013e66bfdb228f5245fa02e51bef
Matthew Harrison <matthewharrison@dyn201183.cc.lehigh.edu>
2023-05-25 16:32:35 -0400

    initial commit of python file

---
+++ VRP_object.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 15:41:30 2023
+
+@author: matthewharrison
+"""
+#Change Directory if Needed:
+#import os
+#os.chdir('/Users/matthewharrison/Documents/VRP_Optimization')
+
+##Initialization
